49f3841b4a50eb517610f46b09089f33ba9aaa41
mtri-git <vmtri20@gmail.com>
2023-07-02 12:32:26 +0700

    first init

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+# route for text classier with underthesea with post method input text
+@app.route('/text_classifier', methods=['POST'])
+def text_classifier():
+    from underthesea import classify
+    text = request.form['text']
+    res = classify(text)
+    return {
+        "category": res[0],
+    }
